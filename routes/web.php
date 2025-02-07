f35d4ffed25dd8ed2dde6430511fab8af161afe5
rodrigoo1604 <rodrigo.lastra.pescador@gmail.com>
2025-02-07 11:57:43 +0100

    feat: added web routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\VehicleController;
+use App\Http\Controllers\TollStationController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/toll-stations', [TollStationController::class, 'index']);
+Route::get('/vehicles', [VehicleController::class, 'index']);
